0759a95d33b8b56e3f97ec8f7a19951a5a2d1245
Peter Levinsky <pele@zealand.dk>
2023-10-13 10:13:05 +0200

    Add project files.

---
+++ Barometer.cs
@@ -0,0 +1,47 @@
+﻿/// <summary>
+///  Simple barometer, measuring pressure in hPa (hectopascal)
+/// </summary>
+public class Barometer
+{
+    #region Properties
+    public double Pressure { get; set; }
+
+    public string WeatherDescription
+    {
+        get { 
+
+            if (Pressure < 980)
+            {
+                return "Stormy";
+            }
+            else // Pressure >= 980
+            if (Pressure <= 1000)
+            {
+                return "Rainy";
+            }
+            else // Pressure > 1000
+            if (Pressure <= 1020)
+            {
+                return "Changing";
+            }
+            else // Pressure > 1020
+            if (Pressure <= 1040)
+            {
+                return "Fair";
+            }
+            else
+            //            if (Pressure > 1040)
+            {
+                return "Very Dry";
+            }
+        }
+    }
+    #endregion
+
+    #region Constructor
+    public Barometer()
+    {
+        Pressure = 1000.0;
+    }
+    #endregion
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Delete this line when you have written your code");
+
+// opretter et objekt af Barometer
+Barometer barometer = new Barometer();
+
+for (int i = 0; i < 7; i++)
+{
+    Console.Write("Skriv lufttrykket (Pressure): "  );
+    string trykStr = Console.ReadLine();
+
+    //int tryk;
+    //if (int.TryParse(trykStr, out tryk))
+    //{
+    //    barometer.Pressure = tryk;
+    //}
+    //else
+    //{
+    //    Console.WriteLine("FEJL");
+    //}
+
+    int tryk = int.Parse(trykStr);
+    barometer.Pressure = tryk;
+
+    string beskrivelse = barometer.WeatherDescription;
+    Console.WriteLine($" Så må det være {beskrivelse} ");
+
+}
